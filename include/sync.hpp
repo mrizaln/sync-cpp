b0bf88d8d42e0cbe010795dc5fae75bd10c0d220
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-01-30 19:48:25 +0700

    Add Sync class that wraps and provides synchronized access to an objct

---
+++ include/sync.hpp
@@ -0,0 +1,136 @@
+#ifndef SYNC_HPP_B4IRUHHF
+#define SYNC_HPP_B4IRUHHF
+
+#include <concepts>
+#include <mutex>
+#include <shared_mutex>
+#include <type_traits>
+#include <utility>
+
+namespace spp
+{
+    template <typename T, typename M = std::mutex>
+        requires(!std::is_reference_v<T> && !std::is_pointer_v<T>)
+             && (std::derived_from<M, std::shared_mutex> || std::derived_from<M, std::mutex>)
+    class Sync
+    {
+    public:
+        using Value_type = T;
+        using Mutex_type = M;
+
+        template <typename... Args>
+            requires std::constructible_from<T, Args...>
+        Sync(Args&&... args)
+            : m_value{ std::forward<Args>(args)... }
+        {
+        }
+
+        explicit Sync(T&& value)
+            : m_value{ std::forward<T>(value) }
+        {
+        }
+
+        // @brief: Public member access of the wrapped value by copy
+        template <typename Mem>
+            requires std::is_member_object_pointer_v<Mem>
+        [[nodiscard]] auto get(Mem mem) const    // the 'auto' removes reference and cv-qualifier
+        {
+            auto lock{ readLock() };
+            return m_value.*mem;
+        }
+
+        // @brief: Convenience function for const member function call of the wrapped value (read-only access)
+        template <typename MemFunc, typename... Args>
+        [[nodiscard]] decltype(auto) read(MemFunc&& f, Args&&... args) const
+            requires std::is_member_function_pointer_v<MemFunc> && std::invocable<MemFunc, const T&, Args...>
+                  && requires(Value_type value) { (value.*f)(std::forward<Args>(args)...); }
+        {
+            if constexpr (std::is_lvalue_reference_v<decltype((m_value.*f)(std::forward<Args>(args)...))>) {
+                static_assert(false && "Member function returning a reference in multithreaded context is dangerous!");
+            }
+
+            auto lock{ readLock() };
+            return (m_value.*f)(std::forward<Args>(args)...);
+        }
+
+        // @brief: Convenience function for non-const member function call of the wrapped value (write access)
+        template <typename MemFunc, typename... Args>
+        [[nodiscard]] decltype(auto) write(MemFunc&& f, Args&&... args)
+            requires std::is_member_function_pointer_v<MemFunc> && std::invocable<MemFunc, T&, Args...>
+                  && requires(Value_type value) { (value.*f)(std::forward<Args>(args)...); }
+        {
+            if constexpr (std::is_lvalue_reference_v<decltype((m_value.*f)(std::forward<Args>(args)...))>) {
+                static_assert(false && "Member function returning a reference in multithreaded context is dangerous!");
+            }
+
+            auto lock{ writeLock() };
+            return (m_value.*f)(std::forward<Args>(args)...);
+        }
+
+        // @brief: Read-only access to the wrapped value
+        template <typename Func>
+            requires(!std::is_member_pointer_v<Func>) && std::invocable<Func, const T&>
+        [[nodiscard]] decltype(auto) read(Func&& f) const
+        {
+            if constexpr (std::is_lvalue_reference_v<decltype(f(m_value))>) {
+                static_assert(false && "Function returning a reference in multithreaded context is dangerous!");
+            }
+
+            auto lock{ readLock() };
+            return f(m_value);
+        }
+
+        // @brief: Write access to the wrapped value
+        template <typename Func>
+            requires(!std::is_member_pointer_v<Func>) && std::invocable<Func, T&>
+        [[nodiscard]] decltype(auto) write(Func&& f)
+        {
+            if constexpr (std::is_lvalue_reference_v<decltype(f(m_value))>) {
+                static_assert(false && "Function returning a reference in multithreaded context is dangerous!");
+            }
+
+            auto lock{ writeLock() };
+            return f(m_value);
+        }
+
+        template <typename TT>
+            requires std::assignable_from<std::decay_t<T>, TT>
+        Sync& operator=(TT&& value)
+        {
+            write([&](auto& v) { v = std::forward<TT>(value); });
+            return *this;
+        }
+
+        // NOTE: at swap, make sure that both 'this' and 'other' does not have any thread waiting for resource from it
+        void swap(Sync& other)
+        {
+            std::unique_lock lock1{ m_mutex, std::defer_lock };
+            std::unique_lock lock2{ other.m_mutex, std::defer_lock };
+
+            std::lock(lock1, lock2);
+
+            std::swap(m_value, other.m_value);
+        }
+
+    private:
+        [[nodiscard]] auto readLock() const
+        {
+            if constexpr (std::derived_from<M, std::shared_mutex>) {
+                return std::shared_lock{ m_mutex };
+            } else {
+                return std::unique_lock{ m_mutex };
+            }
+        }
+
+        [[nodiscard]] auto writeLock() { return std::unique_lock{ m_mutex }; }
+
+        mutable M  m_mutex;
+        Value_type m_value;
+    };
+
+    // deduction guide
+    template <typename T>
+    Sync(T) -> Sync<T>;
+}
+
+#endif /* end of include guard: SYNC_HPP_B4IRUHHF */
---
+++ test/sync_test.cpp
@@ -0,0 +1,446 @@
+#include "sync.hpp"
+#include "print.hpp"
+
+#define ENABLE_OLD_TEST 0
+
+#include <boost/ut.hpp>
+
+#include <algorithm>
+#include <concepts>
+#include <format>
+#include <iostream>
+#include <shared_mutex>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
+#ifndef ENABLE_OLD_TEST
+#    define ENABLE_OLD_TEST 0
+#endif
+
+#ifndef ENABLE_DEADLOCK_CODE
+#    define ENABLE_DEADLOCK_CODE 0
+#endif
+
+#ifndef ENABLE_PRINT
+#    define ENABLE_PRINT 0
+#endif
+
+class Mutex : public std::shared_mutex
+{
+public:
+    Mutex()                        = default;
+    Mutex(const Mutex&)            = delete;
+    Mutex& operator=(const Mutex&) = delete;
+    Mutex(Mutex&&)                 = delete;
+    Mutex& operator=(Mutex&&)      = delete;
+
+    void lock()
+    {
+        print(">>> unique lock\n");
+        std::shared_mutex::lock();
+    }
+
+    void unlock()
+    {
+        print("<<< unique unlock\n");
+        std::shared_mutex::unlock();
+    }
+
+    void lock_shared()
+    {
+        print("+++ shared lock\n");
+        std::shared_mutex::lock_shared();
+    }
+
+    void unlock_shared()
+    {
+        print("--- shared unlock\n");
+        std::shared_mutex::unlock_shared();
+    }
+};
+
+struct AClass
+{
+    int                  m_value;
+    auto                 operator<=>(const AClass&) const = default;
+    friend std::ostream& operator<<(std::ostream& os, const AClass& a)
+    {
+        return os << "AClass { " << a.m_value << " }";
+    }
+};
+
+struct BClass
+{
+    char                 m_value;
+    auto                 operator<=>(const BClass&) const = default;
+    friend std::ostream& operator<<(std::ostream& os, const BClass& b)
+    {
+        return os << "BClass { " << b.m_value << " }";
+    }
+};
+
+class SomeClass
+{
+public:
+    inline static int m_idCounter{ 0 };
+    int               m_id;
+
+    explicit SomeClass(std::string name, int value)
+        : m_id{ ++m_idCounter }
+        , m_name{ std::move(name) }
+        , m_value{ value }
+    {
+        print("\tSomeClass Constructing '{}'\n", m_name);
+    }
+
+    BClass doModification()
+    {
+        m_value++;
+        std::reverse(m_name.begin(), m_name.end());
+        print("\tSomeClass Doing something '{}'\n", m_value);
+        return { m_name.empty() ? '-' : m_name.back() };
+    }
+
+    AClass doConstOperation() const
+    {
+        print("\tSomeClass Doing something const: '{}' | '{}'\n", m_value, m_name);
+        return { m_value };
+    }
+
+    void doSomethingWithArgs(AClass& aClass, int v)
+    {
+        m_value += v - aClass.m_value;
+        print("\tSomeClass Doing something with args: '{}' '{}'\n", m_value, v);
+    }
+
+    std::string concatName(const std::string& name) const { return m_name + name; }
+
+    const std::string& getName() const { return m_name; }
+    std::string        getNameCopy() const { return m_name; }
+
+private:
+    std::string m_name;
+    int         m_value;
+};
+
+template <typename... Args>
+std::string fmt(std::format_string<Args...>&& fmt, Args&&... args)
+{
+    return std::format(std::forward<std::format_string<Args...>>(fmt), std::forward<Args>(args)...);
+};
+
+using namespace spp;
+
+int main()
+{
+    namespace ut = boost::ut;
+    using namespace ut::literals;
+    using namespace ut::operators;
+    using ut::reflection::type_name;
+
+    ut::suite<"Sync Construction"> syncConstruction [[maybe_unused]] = [] {
+        using Resource     = SomeClass;
+        using SyncResource = Sync<SomeClass, Mutex>;
+
+        "Forward construction"_test = [&] {
+            ut::expect(true == std::constructible_from<SyncResource, std::string, int>)
+                << fmt("Construction with '{}' ctor arguments is not possible when it should be!",
+                       type_name<Resource>());
+        };
+
+        "Resource move construction"_test = [&] {
+            ut::expect(true == std::constructible_from<SyncResource, Resource&&>)
+                << fmt("Construction from moved '{}' instance is not possible when it should be!",
+                       type_name<Resource&&>());
+        };
+
+        "Resource copy construction"_test = [&] {
+            ut::expect(true == std::constructible_from<SyncResource, Resource>)
+                << fmt("Construction from copy '{}' is not possible when it should be!", type_name<Resource>());
+        };
+
+        "Sync copy construction"_test = [&] {
+            ut::expect(false == std::constructible_from<SyncResource, SyncResource&>)
+                << fmt("Construction from copy '{}' is possible when it should not!", type_name<SyncResource&>());
+        };
+
+        // // I need to rethink this decision
+        // ut::skip / "Sync Move construction"_test = [&] {
+        //     ut::expect(false == std::constructible_from<SyncResource, SyncResource&&>)
+        //         << fmt("Construction from copy '{}' is possible when it should not!", type_name<SyncResource&&>());
+        // };
+    };
+
+    ut::suite<"Sync Get Member Operation"> syncGetMember [[maybe_unused]] = [] {
+        using Resource     = SomeClass;
+        using SyncResource = Sync<SomeClass, Mutex>;
+
+        SyncResource synced{ "resource", 42 };
+
+        "Get member"_test =
+            [&]<typename Mem>(Mem&& mem) {
+                using Ret      = decltype(std::declval<Resource>().m_id);
+                using RetNoRef = std::remove_reference_t<Ret>;
+
+                using Result = decltype(synced.get(std::move(mem)));
+                ut::expect(true == std::same_as<Ret, Result>)
+                    << fmt("Get member '{}' returns '{}' instead of '{}'",
+                           type_name<Mem>(),
+                           type_name<Result>(),
+                           type_name<RetNoRef>());
+            }
+            | std::tuple{
+                  &Resource::m_id,
+              };
+    };
+
+    ut::suite<"Sync Read Operation"> syncRead [[maybe_unused]] = [] {
+        using Resource     = SomeClass;
+        using SyncResource = Sync<SomeClass, Mutex>;
+
+        SyncResource synced{ "read resource", 42 };
+        Resource     resource{ "read resource", 42 };    // identical to SyncResource stored value
+
+        // I'll skip the invocable test, it's just too hard!
+        "Read using callable"_test =
+            [&]<typename Callable>(Callable&& callable) {
+                using Arg = const Resource&;
+                using Ret = std::invoke_result_t<Callable, Arg>;
+
+                // type equality check
+                using Result = decltype(synced.read(callable));
+                ut::expect(true == std::same_as<Ret, Result>)
+                    << fmt("Read operation using '{}' callable returns '{}' instead of '{}'",
+                           type_name<Callable>(),
+                           type_name<Result>(),
+                           type_name<Ret>());
+
+                // value equality check
+                if constexpr (!std::same_as<Result, void>) {
+                    decltype(auto) result  = synced.read(callable);
+                    decltype(auto) result2 = callable(resource);
+                    ut::expect(ut::that % result == result2) << "Return value should be the same!";
+                }
+            }
+            | std::tuple{
+                  [](const Resource& r) { return r.doConstOperation(); },
+                  [](const Resource& r) { return r.getName(); },
+                  [](auto& r) { return r.getNameCopy(); },    // auto& is deduced to const SomeClass&
+                  [](auto& r) { r.getNameCopy(); },           // void return
+              };
+
+        // I'll skip the invocable test, it's just too hard!
+        "Read using member function (no args)"_test =
+            [&]<typename MemFunc>(MemFunc&& memFunc) {
+                using Arg = const Resource&;
+                using Ret = std::invoke_result_t<MemFunc, Arg>;
+
+                // type equality check
+                using Result = decltype(synced.read(std::move(memFunc)));
+                ut::expect(true == std::same_as<Ret, Result>)
+                    << fmt("Read operation using '{}' member function returns '{}' instead of '{}'",
+                           type_name<MemFunc>(),
+                           type_name<Result>(),
+                           type_name<Ret>());
+
+                // value equality check
+                if constexpr (!std::same_as<Result, void>) {
+                    decltype(auto) result  = synced.read(std::move(memFunc));
+                    decltype(auto) result2 = (resource.*memFunc)();
+                    ut::expect(ut::that % result == result2) << "Return value should be the same!";
+                }
+            }
+            | std::tuple{
+                  &Resource::doConstOperation,
+                  &Resource::getNameCopy,
+              };
+
+        "Read using member function with argument"_test = [&] {
+            using MemFunc = decltype(&Resource::concatName);
+            using ThisArg = Resource&;
+            using Arg     = const std::string&;
+            using Ret     = std::invoke_result_t<MemFunc, ThisArg, Arg>;
+
+            const std::string suffix{ "suffix" };
+
+            // type equality check
+            using Result = decltype(synced.read(&Resource::concatName, suffix));
+            ut::expect(true == std::same_as<Ret, Result>)
+                << fmt("Read operation using '{}' member function returns '{}' instead of '{}'",
+                       type_name<MemFunc>(),
+                       type_name<Result>(),
+                       type_name<Ret>());
+
+            // value equality check
+            if constexpr (!std::same_as<Result, void>) {
+                decltype(auto) result  = synced.read(&Resource::concatName, suffix);
+                decltype(auto) result2 = resource.concatName(suffix);
+                ut::expect(ut::that % result == result2) << "Return value should be the same!";
+            }
+        };
+    };
+
+    ut::suite<"Sync Write Operation"> syncWrite [[maybe_unused]] = [] {
+        using Resource     = SomeClass;
+        using SyncResource = Sync<SomeClass, Mutex>;
+
+        SyncResource synced{ "write resource", 42 };
+        Resource     resource{ "write resource", 42 };    // identical to SyncResource stored value
+
+        // I'll skip the invocable test, it's just too hard!
+        "Write using callable"_test =
+            [&]<typename Callable>(Callable&& callable) {
+                using Arg = Resource&;
+                using Ret = std::invoke_result_t<Callable, Arg>;
+
+                // type equality check
+                using Result = decltype(synced.write(callable));
+                ut::expect(true == std::same_as<Ret, Result>)
+                    << fmt("Write operation using '{}' callable returns '{}' instead of '{}'",
+                           type_name<Callable>(),
+                           type_name<Result>(),
+                           type_name<Ret>());
+
+                // value equality check
+                if constexpr (!std::same_as<Result, void>) {
+                    decltype(auto) result  = synced.write(callable);
+                    decltype(auto) result2 = callable(resource);
+                    ut::expect(ut::that % result == result2) << "Return value should be the same!";
+                }
+            }
+            | std::tuple{
+                  [](Resource& r) { return r.doModification(); },
+                  [](Resource& r) { return r.getName(); },
+                  [](auto& r) { return r.getNameCopy(); },    // auto& is deduced to const SomeClass&
+                  [](auto& r) { r.getNameCopy(); },           // void return
+              };
+
+        "Write using member function (no args)"_test =
+            [&]<typename MemFunc>(MemFunc&& memFunc) {
+                using Arg = Resource&;
+                using Ret = std::invoke_result_t<MemFunc, Arg>;
+
+                // type equality check
+                using Result = decltype(synced.write(std::move(memFunc)));
+                ut::expect(true == std::same_as<Ret, Result>)
+                    << fmt("Write operation using '{}' member function returns '{}' instead of '{}'",
+                           type_name<MemFunc>(),
+                           type_name<Result>(),
+                           type_name<Ret>());
+
+                // value equality check
+                if constexpr (!std::same_as<Result, void>) {
+                    decltype(auto) result  = synced.write(std::move(memFunc));
+                    decltype(auto) result2 = (resource.*memFunc)();
+                    ut::expect(ut::that % result == result2) << "Return value should be the same!";
+                }
+            }
+            | std::tuple{
+                  &Resource::doModification,
+                  &Resource::doConstOperation,
+                  &Resource::getNameCopy,
+              };
+
+        "Write using member function with argument"_test = [&] {
+            using MemFunc = decltype(&Resource::doSomethingWithArgs);
+            using ThisArg = Resource&;
+            using Arg     = int;
+            using Ret     = std::invoke_result_t<MemFunc, ThisArg, AClass&, Arg>;
+
+            AClass aClass{ 12 };
+            int    v{ 42 };
+
+            // type equality check
+            using Result = decltype(synced.write(&Resource::doSomethingWithArgs, aClass, v));
+            ut::expect(true == std::same_as<Ret, Result>)
+                << fmt("Write operation using '{}' member function returns '{}' instead of '{}'",
+                       type_name<MemFunc>(),
+                       type_name<Result>(),
+                       type_name<Ret>());
+        };
+    };
+
+#if ENABLE_OLD_TEST
+    Sync<SomeClass, Mutex> synced{ "SomeClass instance 1", 42 };
+
+    // construction by moving the to-be-synced object
+    SomeClass someClass2{ "SomeClass instance 2", 42 };
+    Sync      synced2{ std::move(someClass2) };    // can't copy the resource, remove std::move to do it
+
+    SomeClass& someClass22{ someClass2 };
+    Sync       synced22{ someClass22 };    // can't copy the resource, remove std::move to do it
+
+    // read from or write to the synced object using lambda
+    std::ignore = synced.read([](auto& v) {    // auto& is deduced to const SomeClass&, I can't constraint this behavior
+        print("[DEBUG] {} at {}: decltype(v) = {}\n", __FILE__, __LINE__, ut::reflection::type_name<decltype(v)>());
+        return v.doConstOperation();
+    });
+    std::ignore = synced.write([](auto& v) {
+        v.doModification();
+        return true;
+    });
+
+    // invoke the member function through read (const) or write (non-const)
+    auto aClass = synced.read(&SomeClass::doConstOperation);    // OK: using read to invoke const member function
+    // std::ignore = synced.read(&SomeClass::doModification);   // FAIL: using read to invoke non-const member function
+
+    std::ignore = synced.write(&SomeClass::doModification);      // OK: using write to invoke non-const member function
+    std::ignore = synced.write(&SomeClass::doConstOperation);    // OK: using write to invoke const member function
+
+    // invoke the member function with arguments (constness matters)
+    synced.write(&SomeClass::doSomethingWithArgs, aClass, 42);    // OK: using write to invoke non-const member function
+    // synced.read(&SomeClass::doSomethingWithArgs, aClass, 42);  // FAIL: using read to invoke non-const member func
+
+    // auto name  = synced.read(&SomeClass::getName);         // FAIL: member function returns a reference
+    // auto name2 = synced.write(&SomeClass::getName);        // FAIL: member function returns a reference
+    auto name  = synced.read(&SomeClass::getNameCopy);     // OK: member function NOT returns a reference
+    auto name2 = synced.write(&SomeClass::getNameCopy);    // OK: member function NOT returns a reference
+
+    auto name3 = synced.read([](const SomeClass& c) { return c.getName(); });         // OK: returns copy
+    auto name4 = synced.write([](const SomeClass& c) { return c.getName(); });        // OK: returns copy
+    auto name5 = synced.read([](const SomeClass& c) { return c.getNameCopy(); });     // OK: returns copy
+    auto name6 = synced.write([](const SomeClass& c) { return c.getNameCopy(); });    // OK: returns copy
+
+    // multiple reader (recursive locks) (will deadlock if using std::mutex but not if using std::shared_mutex)
+    if constexpr (std::derived_from<typename decltype(synced)::Mutex_type, std::shared_mutex>) {
+        synced.read([&synced](const auto& v) {
+            using M = decltype(synced)::Mutex_type;
+            print(
+                "[DEBUG]: recursive locks using {} ({})\n",
+                type_name<M>(),
+                std::derived_from<M, std::shared_mutex> ? "won't deadlock" : "deadlock"
+            );
+            v.doConstOperation();
+            std::ignore = synced.read([](const auto& v) { return v.doConstOperation(); });
+            std::ignore = synced.read(&SomeClass::doConstOperation);
+        });
+    } else {
+        print("[DEBUG]: using std::mutex: skipping recursive lock test\n");
+    }
+
+    // writer and reader deadlock (throws if std::shared_mutex is used)
+    if constexpr (ENABLE_DEADLOCK_CODE) {
+        synced.write([&synced](auto&) {
+            print("[DEBUG]: deadlock! you can't recover from this!\n");
+            std::ignore = synced.read([](const auto& v) { return v.doConstOperation(); });
+            std::ignore = synced.read(&SomeClass::doConstOperation);
+        });
+    } else {
+        print("[DEBUG]: ENABLE_DEADLOCK_CODE is disabled: skipping deadlock test\n");
+    }
+
+    // another writer and reader deadlock (no throws! silent bug!)
+    if constexpr (ENABLE_DEADLOCK_CODE) {
+        synced.read([&synced](const auto&) {
+            print("[DEBUG]: deadlock! you can't recover from this!\n");
+            std::ignore = synced.write(&SomeClass::doModification);
+            std::ignore = synced.read(&SomeClass::doConstOperation);
+        });
+    }
+    {
+        print("[DEBUG]: ENABLE_DEADLOCK_CODE is disabled: skipping deadlock test\n");
+    }
+#endif
+}
